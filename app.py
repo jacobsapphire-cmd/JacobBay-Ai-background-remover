ff9c2a2465077aa3cc9e55f739e96a8af606a083
jacobsapphire-cmd <sapphireajs@gmail.com>
2025-10-27 21:37:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -66,5 +66,8 @@ def progress():
     return jsonify({"progress": 0})
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
